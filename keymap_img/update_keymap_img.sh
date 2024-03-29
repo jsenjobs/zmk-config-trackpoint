a9330021c9f2a5e6ffe582b7c5617b60a6a2bb22
jsen <jsen1922279340@163.com>
2024-03-29 12:29:32 +0800

    更新固件

--- keymap_img/update_keymap_img.sh
+++ keymap_img/update_keymap_img.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo 'Parsing ZMK keymap...'
-keymap -c keymap_config.yaml parse -c 12 -z ../config/corne.keymap > keymap.yaml
+keymap -c keymap_config.yaml parse -c 12 -z ../config/qhws42v3.keymap > keymap.yaml
 
 # echo '\n\nAdjusting keymap yaml...'
 # ./keymap_img_adjuster.py keymap.yaml
 
 echo '\n\nDrawing keymap...'
-keymap -c keymap_config.yaml draw --qmk-keyboard crkbd/rev1 --qmk-layout LAYOUT_split_3x6_3 keymap.yaml -s Base Nav Num Func Sym Adjust > keymap.svg
+keymap -c keymap_config.yaml draw --qmk-keyboard crkbd/rev1 --qmk-layout LAYOUT_split_3x6_3 keymap.yaml -s BASE_QWERTY NUM_NAV SYMBOL FUNC_MOUSEKEY MEDIA_EXT DEBUG SYS > keymap.svg
